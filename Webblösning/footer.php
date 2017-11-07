8d66219aa9c5fc40e01862a28ea172b5a1229c1a
Karl Viktor <Bergstrom.viktor@gmail.com>
2017-11-07 14:23:53 +0100

    Update footer.php

--- Webblösning/footer.php
+++ Webblösning/footer.php
@@ -1,4 +1,5 @@
 <footer>
+<!--Hanterar headern som visas på varje sida i applikationen, Är en separat fil för att undvika repitativ kod-->	
 	<h3>KONTAKTINFORMATION / SOCIALA MEDIER</h3>
 	<p><b>E-mail:</b> <a href="mailto:adress?" target="_top">MAIL</a></p>
 	
